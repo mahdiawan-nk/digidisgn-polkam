574d66d52ac4eda6d2a30017f78fb60a60f956e0
mahdiawan-nk <dev.mrcoders@gmail.com>
2025-01-29 03:40:21 +0700

    Update document_sign.blade.php

--- resources/views/document_sign.blade.php
+++ resources/views/document_sign.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>Document Sign || {{ config('app.name') }}</title>
+    <title>POLITEKNIK KAMPAR || DIGSIGN</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
 
